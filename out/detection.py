9858193b722c917249ceee04984aa8b61194af30
Gringo Suave <mixmastamyk@bitbucket.org>
2024-10-30 13:02:31 -0700

    fix detect, use stderr. support envars

--- out/detection.py
+++ out/detection.py
@@ -9,12 +9,33 @@ from console.style import ForegroundPalette, BackgroundPalette, EffectsPalette
 
 def _find_palettes(stream):
     ''' Need to configure palettes manually, since we are checking stderr. '''
-    level = init(_stream=stream)
-    fg = ForegroundPalette(level=level)
-    bg = BackgroundPalette(level=level)
-    fx = EffectsPalette(level=level)
+    # doing this manually because we're using std-error, may be other reasons
+    is_tty = is_a_tty(stream)
 
-    return fg, bg, fx, level, is_a_tty(stream)
+    try:  # avoid install issue with new pip:2024 :-/
+        import env
+    except ModuleNotFoundError as err:
+        print(str(err))
+
+    # detection is performed if not explicitly disabled
+    if is_tty and (
+        env.PY_CONSOLE_AUTODETECT.value is None or
+        env.PY_CONSOLE_AUTODETECT.truthy
+    ):
+        level = init(_stream=stream)
+        fg = ForegroundPalette(level=level)
+        bg = BackgroundPalette(level=level)
+        fx = EffectsPalette(level=level)
+
+    else:
+        from console.constants import TermLevel
+        from console.disabled import empty_bin as _empty_bin
+
+        # Define pass-thru palette objects for streams and dumb terminals:
+        level = TermLevel.DUMB
+        fg = bg = fx = _empty_bin
+
+    return fg, bg, fx, level, is_tty
 
 
 TermLevel, is_fbterm, os_name  # quiet pyflakes
